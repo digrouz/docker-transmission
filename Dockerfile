f8067570e2800496fc664099224ce1be6f12be15
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2016-09-16 08:27:01 +0000

    initial version

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# vim:set ft=dockerfile:
+FROM alpine:latest
+MAINTAINER DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
+
+### Environment variables
+ENV LANG='en_US.UTF-8' \
+    LANGUAGE='en_US.UTF-8' \
+    TERM='xterm' 
+
+### Install Application
+RUN apk upgrade --no-cache && \
+    apk add --no-cache --virtual=run-deps \
+      su-exec \
+      transmission-daemon && \
+    rm -rf /tmp/* \
+           /var/cache/apk/*  \
+           /var/tmp/*
+    
+### Volume
+VOLUME ["/config","/downloads","/watch"]
+
+### Expose ports
+EXPOSE 9091 9092/tcp 9092/udp
+
+### Running User: not used, managed by docker-entrypoint.sh
+#USER transmission
+
+### Start transmission
+COPY ./docker-entrypoint.sh /
+ENTRYPOINT ["/docker-entrypoint.sh"]
+CMD ["transmission"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+local MYUSER="mytransmission"
+local MYGID="10003"
+local MYUID="10003"
+
+#Managing group
+if [ -n "${DOCKGID}" ]; then
+  MYGID="${DOCKGID}"
+fi
+if ! /bin/grep -q "${MYUSER}" /etc/group; then
+  /usr/sbin/addgroup -S -g "${MYGID}" "${MYUSER}"
+fi
+
+#Managing user
+if [ -n "${DOCKUID}" ]; then
+  MYUID="${DOCKUID}"
+fi
+if ! /bin/grep -q "${MYUSER}" /etc/passwd; then
+  /usr/sbin/adduser -S -D -H -G "${MYUSER}" -u "${MYUID}" "${MYUSER}"
+fi
+
+if [ "$1" = 'transmission' ]; then
+    if [ -d /config ]; then
+      chown -R "${MYUSER}":"${MYUSER}" /config
+      chmod 0775 /config
+    fi
+    exec /sbin/su-exec "${MYUSER}" "/usr/bin/transmission-daemon --foreground --config-dir /config -c /watch -w /downloads -p 9091  -P 9092 --log-error
+fi
+
+exec "$@"

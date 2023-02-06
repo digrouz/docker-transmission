1cb8b645ff64d05a34f9a8d95f08ac6485b229f8
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2023-02-06 07:44:26 +0000

    update to version:

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -2,7 +2,11 @@
 
 TRANSMISSION_URL="https://api.github.com/repos/transmission/transmission/tags"
 
-LAST_VERSION=$(curl -SsL ${TRANSMISSION_URL} | jq .[0].name -r )
+FULL_LAST_VERSION=$(curl -SsL ${TRANSMISSION_URL} | \
+              jq -r -c '.[] | select( .prerelease == false ) | .tag_name' |\
+              head -1 \
+              )
+LAST_VERSION="${FULL_LAST_VERSION:1}"
 
 sed -i -e "s|TRANSMISSION_VERSION='.*'|TRANSMISSION_VERSION='${LAST_VERSION}'|" Dockerfile*
 

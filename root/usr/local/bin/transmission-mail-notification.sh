2d6f06e6d2618198a14844c75a78730979fb880b
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2025-03-26 07:30:45 +0000

    added console message

--- root/usr/local/bin/transmission-mail-notification.sh
+++ root/usr/local/bin/transmission-mail-notification.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env sh
 mail -s "Transfer completed" root <<EOF
 ${TR_TORRENT_NAME} is finished.This is an automatic message from your friendly transmission-daemon (${TR_APP_VERSION}). Your files are in ${TR_TORRENT_DIR}.
 EOF
+
+# Prints a info message in the console
+echo "[${TR_TIME_LOCALTIME}]-[${TR_TORRENT_NAME}] Download completed. Email notification sent."

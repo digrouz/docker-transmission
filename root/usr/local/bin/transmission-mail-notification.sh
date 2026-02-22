cfb6e10e4cd90ebc7d19861054c2f9b1c2d829f1
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2026-02-22 13:33:34 +0100

    Update transmission mail notification script

--- root/usr/local/bin/transmission-mail-notification.sh
+++ root/usr/local/bin/transmission-mail-notification.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-mail -s "Transfer completed" root <<EOF
+mail -s "[Transmission] Transfer completed" root <<EOF
 ${TR_TORRENT_NAME} is finished.This is an automatic message from your friendly transmission-daemon (${TR_APP_VERSION}). Your files are in ${TR_TORRENT_DIR}.
 EOF
 
